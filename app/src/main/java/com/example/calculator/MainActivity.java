da990c8a7c82b11bab17218064c5242595579300
sotogito <108535526+sotogito@users.noreply.github.com>
2024-01-23 16:44:58 +0900

    feat : 계산기 기본 변수 선언 첫 입력값, 결과, 중간 연산자

--- app/src/main/java/com/example/calculator/MainActivity.java
+++ app/src/main/java/com/example/calculator/MainActivity.java
@@ -3,12 +3,21 @@ package com.example.calculator;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    boolean isFirstInput = true;
+    int resultNumber = 0; //인터페이스 사용
+    char operator = '+';
+
+    TextView resultText; //ui를 구현하면서 생성한 버튼 id를 부여할 수 있다. 단, 프로그램이 실행된 후에 부여 가능하다.
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        resultText = findViewById(R.id.result_text);
     }
 }
